3ca98b35c4aa5af7880b77785a311a12f40cdd2e
Basil Vazhathottathil <basilveldose@gmail.com>
2025-03-27 03:52:08 +0530

    looks are done

--- src/app/components/card/Card.tsx
+++ src/app/components/card/Card.tsx
@@ -4,10 +4,19 @@ import { Textarea } from "@/components/ui/textarea"
 
 function Card() {
   return (
-    <div>
-        <Textarea/>
-        <Button>choose your csv file</Button>
-        <Button>Send messages</Button>
+    <div className="bg-gray-500 p-8 rounded-lg shadow-md w-[400px] space-y-4 border-black border-2">
+      <div className='flex flex-col space-y-4'>
+        <Textarea className="w-full bg-white border-black border-2"/>
+        <div className="flex items-center gap-4 justify-start">
+          <Button className="w-40">choose your csv file</Button>
+          <label className="text-sm">name</label>
+        </div>
+        
+        <div className="flex items-center gap-4 justify-start">
+          <Button className="w-40">start sending</Button>
+          <label className="text-sm">done?</label>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import Card from "./components/card/Card";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col justify-center items-center min-h-screen"
+      style={{backgroundImage: "url('/bg.jpg')"}}>
     <Card/>
-    </>
+    </div>
   );
 }
