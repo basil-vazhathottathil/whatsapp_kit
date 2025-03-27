c3e346798c836dbaf008f5e1ea273bf4d9f1cdcd
Basil Vazhathottathil <basilveldose@gmail.com>
2025-03-27 14:26:37 +0530

    file select done

--- src/app/components/card/Card.tsx
+++ src/app/components/card/Card.tsx
@@ -24,22 +24,29 @@ function Card() {
     }
   };
 
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
   return (  
     <div className="bg-gray-500 p-8 rounded-lg shadow-md w-[400px] space-y-4 border-black border-2">
       <div className='flex flex-col space-y-4'>
-        <input type="file"
+        <input 
+          type="file"
           accept='.csv'
           onChange={handleFileSelect}
           ref={fileInputRef}
+          style={{ display: 'none' }}  // Hide the default input
         />
-        <Textarea className="w-full bg-white border-black border-2"
+        <Textarea 
+          className="w-full bg-white border-black border-2"
           placeholder='enter the message to be sent here'
           value={message}
           onChange={(e)=> setMessage(e.target.value)}
-          />
+        />
         <div className="flex items-center gap-4 justify-start">
-          <Button className="w-40">choose your file</Button>
-          <label className='text-sm'>enter file name</label>
+          <Button className="w-40" onClick={handleButtonClick}>choose your file</Button>
+          <label className='text-sm'>{filename || 'No file selected'}</label>
         </div>
         
         <div className="flex items-center gap-4 justify-start">
